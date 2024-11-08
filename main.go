86f123cba059a0cd9ee4743a18b80472f72c88a2
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-11-08 10:13:06 +0100

    feat(base): creation and start http server

--- main.go
+++ main.go
@@ -1,5 +1,60 @@
 package golatt
 
-func Setup() {
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"io/fs"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
 
+// Golatt is a http server empowered by mux.Router
+type Golatt struct {
+	Templates fs.FS
+	*mux.Router
+}
+
+// New creates a new Golatt instance with provided files (must be valid go templates files)
+func New(files fs.FS) *Golatt {
+	return &Golatt{
+		Templates: files,
+		Router:    mux.NewRouter(),
+	}
+}
+
+// StartServer starts the http server listening on addr (e.g. ":8000", "127.0.0.1:80")
+func (g *Golatt) StartServer(addr string) {
+	g.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./public"))))
+	g.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./dist"))))
+
+	srv := &http.Server{
+		Handler:      g,
+		Addr:         addr,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	slog.Info("Starting...")
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			slog.Error(err.Error())
+		}
+	}()
+
+	slog.Info("Started")
+	slog.Info("Listening on " + srv.Addr)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	err := srv.Shutdown(ctx)
+	if err != nil {
+		panic(err)
+	}
+	slog.Info("Shutting down")
 }
